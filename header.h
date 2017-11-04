1820bf33f475aa91f02b16f4c0c6df4937551b2c
Diogo Amores <damores@student.dei.uc.pt>
2017-11-04 03:02:32 +0000

    Thread, basic structs and reading from file implemented

---
+++ header.h
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*Server.c*/
+void read_from_file();
+
+/*Estrutura para config.txt
+//triage -> nthreads
+//doctors -> nprocesses
+//shift -> Processes run time
+//mq_max -> Maximum queue size*/
+
+typedef struct config{
+    int triage;
+    int doctors;
+    float shift_length;
+    int mq_max;
+}config;
+
+/*Cria pointer para estrutura config*/
+config *config_ptr;
+/*Aloca memória para a estrutura*/
+
+/*Estrutua paciente
+//name -> nome do paciente
+//triage_time -> tempo de triagem
+//service_time -> tempo de atendimento*/
+
+typedef struct patient{
+    /*por agora vou por aqui um char estatico just cuz*/
+    char name[50];
+    float triage_time;
+    float service_time;
+}patient;
+
+/*Cria pointer para estrutura patient*/
+patient *patient_ptr;
+
+/*Estrutura para as estatísticas
+//num_triage -> Número de pacientes triados
+//num_service -> Número de pacientes atendidos
+//wait_btime -> Tempo médio de espera antes do início triagem
+//wait_etime -> Tempo médio de espera entre o fim da triagem e
+//o início do atendimento
+//wait_time -> Média do tempo total que cada paciente gastou desde
+//que chegou ao sistema até sair*/
+
+typedef struct stats{
+    int num_triage;
+    int num_service;
+    float wait_btime;
+    float wait_etime;
+    float wait_time;
+}stats;
+
+/*Criar pointer para statistics*/
+stats *stats_ptr;
---
+++ main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+#include <string.h>
+#include <pthread.h>
+
+/*Função que le do ficheiro e armazena na estrutura*/
+void read_from_file(){
+    FILE *fp = fopen("config.txt", "r");
+    if (!fp){
+        printf("Error reading the file\n");
+        exit(0);
+    }
+    else
+        printf("Sucessfully read the file\n");
+    char triage[10], doctors[10], shift[10], mq[10];
+
+    fscanf(fp, "TRIAGE=%s\n", triage);
+    fscanf(fp, "DOCTORS=%s\n", doctors);
+    fscanf(fp, "SHIFT_LENGTH=%s\n", shift);
+    fscanf(fp, "MQ_MAX=%s\n", mq);
+
+    (config_ptr->triage) = atoi(triage);
+    (config_ptr->doctors) = atoi(doctors);
+    (config_ptr->shift_length) = atoi(shift);
+    (config_ptr->mq_max) = atoi(mq);
+    fclose(fp);
+
+    /*Teste*/
+    printf("Numero de threads (Triage): %d\n", config_ptr->triage);
+    printf("Numero de processos (Doctors): %d\n", config_ptr->doctors);
+    printf("Duracao do shift (Shift Length): %f\n", config_ptr->shift_length);
+    printf("Message queue (MQ): %d\n", config_ptr->mq_max);
+}
+void *worker(){
+    printf("Triage thread\n");
+    sleep(2);
+    printf("Exiting...\n");
+    pthread_exit(NULL);
+}
+
+void thread_pool(){
+    int i=0;
+    pthread_t triage_thread[config_ptr->triage];
+    int triage_id[config_ptr->triage];
+    for(i=0; i<config_ptr->triage; i++){
+        triage_id[i] = i;
+        if(pthread_create(&triage_thread[i], NULL, worker, &triage_id[i])==0){
+            printf("Triage thread %d was created\n", triage_id[i]);
+        }
+        else{
+            perror("Error creating Triage thread\n");
+        }
+    }
+    for(i=0; i<config_ptr->triage; i++){
+        if(pthread_join(triage_thread[i], NULL)==0){
+            printf("Triage thread %d was killed\n", triage_id[i]);
+        }
+        else{
+            perror("Error killing Triage thread\n");
+        }
+    }
+
+}
+
+int main(){
+    config_ptr = malloc(sizeof(config));
+    if(config_ptr == NULL){
+        printf("Memory allocation error\n");
+        return(-1);
+    }else{
+        printf("Memory successfully allocated\n");
+        }
+    read_from_file();
+    thread_pool();
+    printf("Exiting...\n");
+}
